2389059ea7297c91834b457547fa557b0a501ba4
Anatolii <xottabut@gmail.com>
2014-04-29 15:18:40 +0300

    small refactor CreateCircuite

--- InternetProvider/InternetProvider-web/src/main/java/com/netcracker/wind/commands/implementations/pedashboard/CreateCircuit.java
+++ InternetProvider/InternetProvider-web/src/main/java/com/netcracker/wind/commands/implementations/pedashboard/CreateCircuit.java
@@ -10,14 +10,10 @@ import com.netcracker.wind.dao.factory.AbstractFactoryDAO;
 import com.netcracker.wind.dao.factory.FactoryCreator;
 import com.netcracker.wind.dao.interfaces.ICircuitDAO;
 import com.netcracker.wind.dao.interfaces.IPortDAO;
-import com.netcracker.wind.dao.interfaces.IServiceInstanceDAO;
 import com.netcracker.wind.dao.interfaces.ITaskDAO;
 import com.netcracker.wind.entities.Circuit;
-import com.netcracker.wind.entities.Port;
 import com.netcracker.wind.entities.Task;
 import com.netcracker.wind.workflow.Workflow;
-import static com.netcracker.wind.workflow.Workflow.createTaskForIE;
-import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -27,8 +23,6 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class CreateCircuit implements ICommand {
 
-//    private static final String PORT_ID = "port_id";
-//    private static final String SERVICE_INSTANCE_ID = "service_instance_id";
     private static final String TASK_ID = "task_id";
 
     @Override
@@ -54,17 +48,10 @@ public class CreateCircuit implements ICommand {
         int serviceInstanceID = task.getServiceOrder().getServiceInstance().getId();
 
         Circuit circuit = circuitDAO.findByServInst(serviceInstanceID);
-        Port port = circuit.getPort();
-        if (port == null) {
-            Port newPort = portDAO.occupyFreePort();
-            if (newPort == null) {
-                Workflow.createTaskForIE(
-                        circuit.getServiceInstance().getServiceOrder(),
-                        Task.TaskType.NEW_DEVICE,
-                        taskDAO);
-            }
-        }
-
+        //TODO setting circuit
+        task.setStatus(Task.TaskStatus.COMPLETED.toString());
+        taskDAO.update(task);
+        Workflow.createTaskForCSE(task.getServiceOrder(), taskDAO);
         //TODO return next page
         return "";
     }
