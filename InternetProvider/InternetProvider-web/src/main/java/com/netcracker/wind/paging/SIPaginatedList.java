f72d2ea3a1c05224a3530235b3c2e0d6add59789
panotchesashko <sergiyenko.olexandr@gmail.com>
2014-05-03 20:51:06 +0300

    new

--- InternetProvider/InternetProvider-web/src/main/java/com/netcracker/wind/paging/SIPaginatedList.java
+++ InternetProvider/InternetProvider-web/src/main/java/com/netcracker/wind/paging/SIPaginatedList.java
@@ -34,38 +34,3 @@ public class SIPaginatedList extends AbstractPaginatedList {
     }
 
 }
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package com.netcracker.wind.paging;
-
-import com.netcracker.wind.dao.factory.AbstractFactoryDAO;
-import com.netcracker.wind.dao.factory.FactoryCreator;
-import com.netcracker.wind.dao.interfaces.IServiceInstanceDAO;
-import java.util.List;
-import javax.servlet.http.HttpServletRequest;
-
-/**
- *
- * @author Anatolii
- */
-public class SIPaginatedList extends AbstractPaginatedList {
-
-    private final IServiceInstanceDAO serviceInstanceDAO
-            = FactoryCreator.getInstance().getFactory().createServiceInstanceDAO();
-
-    public SIPaginatedList(HttpServletRequest request, int pageSize) {
-        super(request, pageSize);
-    }
-
-    public List getList() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    public int getFullListSize() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-}
