6f3a16978624836fbd7358934c50f54173c72a49
OksanaProtsyk <oksana.protsyk11@yandex.ua>
2014-05-04 17:45:06 +0300

    cse task new paging

--- InternetProvider/InternetProvider-web/src/main/java/com/netcracker/wind/commands/implementations/csedashboard/CSEGetGroupTasks.java
+++ InternetProvider/InternetProvider-web/src/main/java/com/netcracker/wind/commands/implementations/csedashboard/CSEGetGroupTasks.java
@@ -8,19 +8,31 @@ package com.netcracker.wind.commands.implementations.csedashboard;
 import com.netcracker.wind.commands.DashboardsUtilities;
 import com.netcracker.wind.commands.ICommand;
 import com.netcracker.wind.entities.Role;
+import com.netcracker.wind.paging.IExtendedPaginatedList;
+import com.netcracker.wind.paging.TaskPaginationList;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  *
  * @author Oksana
  */
 public class CSEGetGroupTasks implements ICommand {
+      private static final String TASKS = "tasks";
 
+     
     public String execute(HttpServletRequest request, HttpServletResponse response) {
-       int number = Integer.parseInt(request.getParameter("size"));
-       int from = Integer.parseInt(request.getParameter("from"));
-       return DashboardsUtilities.getGroupTaskJSON(Role.CSE_GROUP_ID, from, number);
+       IExtendedPaginatedList paginatedList = new TaskPaginationList(request, 
+                IExtendedPaginatedList.DEFAULT_PAGE_SIZE).setGroup(Role.CSE_GROUP_ID);
+        HttpSession session = request.getSession(false);
+        if(session == null){
+            return "";
+        }
+        session.setAttribute(TASKS, paginatedList);
+        return "/WEB-INF/cse/?";
+
     }
+        
 
 }
