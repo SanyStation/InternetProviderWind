d011a7df3c4e9ef5a856d62768bc7ecdb8fa7708
Anatolii <xottabut@gmail.com>
2014-05-05 14:37:21 +0300

    CSE SendBill

--- InternetProvider/InternetProvider-web/src/main/java/com/netcracker/wind/commands/implementations/csedashboard/SentBill.java
+++ InternetProvider/InternetProvider-web/src/main/java/com/netcracker/wind/commands/implementations/csedashboard/SentBill.java
@@ -3,16 +3,21 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.netcracker.wind.commands.implementations.csedashboard;
 
 import com.netcracker.wind.commands.ICommand;
+import com.netcracker.wind.dao.factory.AbstractFactoryDAO;
 import com.netcracker.wind.dao.factory.FactoryCreator;
+import com.netcracker.wind.dao.interfaces.IServiceInstanceDAO;
+import com.netcracker.wind.dao.interfaces.IServiceOrderDAO;
 import com.netcracker.wind.dao.interfaces.ITaskDAO;
+import com.netcracker.wind.entities.ServiceInstance;
+import com.netcracker.wind.entities.ServiceOrder;
 import com.netcracker.wind.entities.Task;
 import com.netcracker.wind.entities.User;
 import com.netcracker.wind.mail.FormatedMail;
 import com.netcracker.wind.mail.MailSendler;
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
 import javax.servlet.http.HttpServletRequest;
@@ -23,26 +28,41 @@ import javax.servlet.http.HttpSession;
  *
  * @author Oksana
  */
-public class SentBill implements ICommand{
+public class SentBill implements ICommand {
 
     public String execute(HttpServletRequest request, HttpServletResponse response) {
-         HttpSession session = request.getSession(false);
-        if(session == null){
+        HttpSession session = request.getSession(false);
+        if (session == null) {
             return "";
         }
         User user = (User) session.getAttribute("user");
         if (user == null) {
             return "";
         }
-        Task task = (Task) session.getAttribute("task");
+        AbstractFactoryDAO factoryDAO = FactoryCreator.getInstance().getFactory();
+        ITaskDAO taskDAO = factoryDAO.createTaskDAO();
+        IServiceOrderDAO orderDAO = factoryDAO.createServiceOrderDAO();
+        IServiceInstanceDAO serviceInstanceDAO = factoryDAO.createServiceInstanceDAO();
+
+        int taskId = Integer.parseInt(request.getParameter("task_id"));
+
+        Task task = taskDAO.findById(taskId);
         List<User> users = new ArrayList<User>();
-                users.add(user);
-                
-         new MailSendler().sendEmail(users, "Boreas Bill", new FormatedMail().getSentBillMassage(null, null, user));
-         task.setStatus(Task.Status.COMPLETED);
-         ITaskDAO taskDAO = FactoryCreator.getInstance().getFactory().createTaskDAO();
-         taskDAO.update(task); 
-        return "";
+        users.add(user);
+
+        task.setStatus(Task.Status.COMPLETED);
+
+        ServiceOrder order = task.getServiceOrder();
+        ServiceInstance serviceInstance = order.getServiceInstance();
+        order.setCompletedate(new Timestamp(System.currentTimeMillis()));
+        order.setStatus(ServiceOrder.Status.COMPLETED);
+        orderDAO.update(order);
+        serviceInstance.setStatus(ServiceInstance.Status.ACTIVE);
+        serviceInstanceDAO.update(serviceInstance);
+        taskDAO.update(task);
+        request.setAttribute("task", task);
+//        new MailSendler().sendEmail(users, "Boreas Bill", new FormatedMail().getSentBillMassage(null, null, user));
+        return "/WEB-INF/cse/cse-page-selected-task.jsp";
     }
-    
+
 }
--- InternetProvider/InternetProvider-web/src/main/java/com/netcracker/wind/entities/ServiceOrder.java
+++ InternetProvider/InternetProvider-web/src/main/java/com/netcracker/wind/entities/ServiceOrder.java
@@ -22,7 +22,7 @@ public class ServiceOrder implements Serializable {
     
     public static enum Status {
         
-        ENTERING, PROCESSING, CANCELLED;
+        ENTERING, PROCESSING, CANCELLED, COMPLETED;
         
         public String getStatus(){
             return toString();
